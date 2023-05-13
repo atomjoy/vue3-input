ce3cc1952c7c9297058e166e8a4e28f8c7e7f398
atomjoy <atomjoy.official@gmail.com>
2023-05-13 15:47:45 +0200

    Button

---
+++ input/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+let props = defineProps(['text', 'class'])
+</script>
+
+<template>
+	<button :class="props?.class">{{ props?.text ?? 'Submit' }}</button>
+</template>
+
+<style>
+button {
+	float: right;
+	margin-top: 20px;
+	padding: 16px 35px;
+	color: #fff;
+	background: var(--wow-accent);
+	border-radius: var(--wow-border-radius);
+	border: 0px;
+	font-size: 16px;
+	font-weight: 500;
+	cursor: pointer;
+	transition: all 0.6s;
+}
+
+button:hover {
+	filter: brightness(1.5);
+}
+</style>
