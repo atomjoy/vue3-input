02db0c45dd945dcc405ef4579ba01c54a14dbb41
Atomjoy <atomjoy.official@gmail.com>
2023-07-31 22:15:07 +0200

    Avatar upload

---
+++ input/AvatarInput.vue
@@ -0,0 +1,92 @@
+<script setup>
+import axios from 'axios'
+import Input from '@/components/input/Input.vue'
+import { toRefs } from 'vue'
+
+const props = defineProps({
+	image: { default: 'https://i.pravatar.cc/128' },
+	label: { default: 'Select image' },
+	remove_avatar_url: { default: 'web/api/remove/avatar' },
+})
+
+const { image } = toRefs(props)
+
+function getImagePath(e) {
+	const path = URL.createObjectURL(e.target.files[0])
+	if (path) {
+		image.value = path
+	}
+}
+
+async function removeAvatar() {
+	try {
+		await axios.get(props.remove_avatar_url)
+		alert('Avatar removed.')
+	} catch (err) {
+		alert('Avatar not removed.')
+	}
+}
+</script>
+
+<template>
+	<div class="input-group">
+		<div class="avatar-input">
+			<div @click="removeAvatar" class="delete-avatar" :title="$t('Remove avatar')">
+				<svg width="24px" height="24px" viewBox="0 0 24.75 24.75">
+					<path
+						d="M9.87787 4.24993C10.1871 3.37503 11.0215 2.75 12 2.75C12.9785 2.75 13.813 3.37503 14.1222 4.24993C14.2602 4.64047 14.6887 4.84517 15.0793 4.70713C15.4698 4.56909 15.6745 4.1406 15.5365 3.75007C15.022 2.29459 13.634 1.25 12 1.25C10.3661 1.25 8.97804 2.29459 8.46361 3.75007C8.32557 4.1406 8.53026 4.56909 8.9208 4.70713C9.31134 4.84517 9.73983 4.64047 9.87787 4.24993Z" />
+					<path d="M2.75 6C2.75 5.58579 3.08579 5.25 3.5 5.25H20.5001C20.9143 5.25 21.2501 5.58579 21.2501 6C21.2501 6.41421 20.9143 6.75 20.5001 6.75H3.5C3.08579 6.75 2.75 6.41421 2.75 6Z" />
+					<path
+						d="M5.11686 7.75166C5.53015 7.72411 5.88753 8.03681 5.91508 8.45011L6.37503 15.3492C6.46488 16.6971 6.52891 17.6349 6.66948 18.3405C6.80583 19.025 6.99616 19.3873 7.26957 19.6431C7.54299 19.8988 7.91715 20.0647 8.60915 20.1552C9.32255 20.2485 10.2626 20.25 11.6134 20.25H12.3868C13.7376 20.25 14.6776 20.2485 15.391 20.1552C16.083 20.0647 16.4572 19.8988 16.7306 19.6431C17.004 19.3873 17.1943 19.025 17.3307 18.3405C17.4712 17.6349 17.5353 16.6971 17.6251 15.3492L18.0851 8.45011C18.1126 8.03681 18.47 7.72411 18.8833 7.75166C19.2966 7.77921 19.6093 8.13659 19.5818 8.54989L19.1183 15.5016C19.0328 16.7844 18.9637 17.8205 18.8018 18.6336C18.6334 19.4789 18.347 20.185 17.7554 20.7384C17.1638 21.2919 16.4402 21.5307 15.5856 21.6425C14.7635 21.75 13.7251 21.75 12.4395 21.75H11.5607C10.2751 21.75 9.23663 21.75 8.41459 21.6425C7.55994 21.5307 6.83639 21.2919 6.2448 20.7384C5.6532 20.185 5.36678 19.4789 5.19838 18.6336C5.03641 17.8205 4.96735 16.7844 4.88186 15.5016L4.41841 8.54989C4.39085 8.13659 4.70356 7.77921 5.11686 7.75166Z" />
+				</svg>
+			</div>
+
+			<img :src="image" class="avatar-image" />
+
+			<Input @change="getImagePath" :label="$t(props.label)" name="image" type="file" />
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.avatar-input {
+	position: relative;
+	display: flex;
+	float: left;
+	width: 100%;
+}
+.delete-avatar {
+	position: absolute;
+	top: 60px;
+	left: 60px;
+	width: 30px;
+	height: 30px;
+	padding: 3px;
+	border-radius: 50%;
+	cursor: pointer;
+	color: #fff;
+	border: 2px solid var(--bg-primary);
+	background: var(--accent-primary);
+}
+.delete-avatar svg {
+	width: 20px;
+	height: 20px;
+	fill: #fff;
+}
+.delete-avatar:hover {
+	background: #f23;
+}
+.avatar-image {
+	float: left;
+	min-width: 80px;
+	max-width: 80px;
+	min-height: 80px;
+	max-height: 80px;
+	padding: 3px;
+	margin-right: 20px;
+	margin-top: 10px;
+	border-radius: 50%;
+	object-fit: cover;
+	border: 2px solid var(--accent-primary);
+}
+</style>
